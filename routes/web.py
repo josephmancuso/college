eeb03db1631dad33d739b757c790ef02806b875f
Joseph Mancuso <idmann509@gmail.com>
2018-04-29 21:35:53 -0400

    initial commit

---
+++ routes/web.py
@@ -0,0 +1,33 @@
+''' Web Routes '''
+from masonite.routes import Get, Post
+
+ROUTES = [
+    Get().route('/', 'WelcomeController@show').name('welcome'),
+]
+
+ROUTES = ROUTES + [
+    Get().route('/login', 'LoginController@show'),
+    Get().route('/logout', 'LoginController@logout'),
+    Post().route('/login', 'LoginController@store'),
+    Get().route('/register', 'RegisterController@show'),
+    Post().route('/register', 'RegisterController@store'),
+    Get().route('/home', 'HomeController@show'),
+]
+
+ROUTES = ROUTES + [
+    Get().route('/login', 'LoginController@show'),
+    Get().route('/logout', 'LoginController@logout'),
+    Post().route('/login', 'LoginController@store'),
+    Get().route('/register', 'RegisterController@show'),
+    Post().route('/register', 'RegisterController@store'),
+    Get().route('/home', 'HomeController@show'),
+]
+
+ROUTES = ROUTES + [
+    Get().route('/login', 'LoginController@show'),
+    Get().route('/logout', 'LoginController@logout'),
+    Post().route('/login', 'LoginController@store'),
+    Get().route('/register', 'RegisterController@show'),
+    Post().route('/register', 'RegisterController@store'),
+    Get().route('/home', 'HomeController@show'),
+]
